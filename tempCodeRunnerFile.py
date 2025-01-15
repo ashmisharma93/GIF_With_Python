9bdf965c2012a19f5487fabc9a8089da0361fa66
ashmisharma93 <as.ashmitasharma93@gmail.com>
2025-01-15 19:27:47 +0530

    Add missing GIF and code file

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+image1 = Image.open("Pikachu-1.jpg")
+# image2 = Image.open("Pikachu-2.jpg")
+
+# size = (500,500)
+# image1_resized = image1.resize(size)
+# image2_resized = image2.resize(size)
+
+# image1_resized.save("image1_resized.jpg")
+# image2_resized.save("image2_resized.jpg")
+
+# print("Images resized successfully!")
